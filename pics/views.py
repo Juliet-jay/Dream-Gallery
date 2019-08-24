0fced34af6477ad5f76b8f7eacb557958b277eb6
kathure <juliekmuthee@gmail.com>
2019-08-24 18:04:29 +0300

    add get all image as a request

--- pics/views.py
+++ pics/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+def index(request):
+    images = Image.get_all_images()
+    locations = Location.get_locations()
+    categories = Category.get_all_categories()
+    return render(request,'index.html',{'images':images, 'categories':categories, 'locations':locations})
+
+
+
