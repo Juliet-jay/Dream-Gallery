6ebeaa9c5c697fbe10ae45b210bb6776594a9a4d
kathure <juliekmuthee@gmail.com>
2019-08-25 13:42:15 +0300

    fixed syntax error

--- pics/urls.py
+++ pics/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
-urlpatterns=[
-    url('^$',views.index,name = 'index'),
+urlpatterns = [
+    url(r'^$',views.gallery,name = 'gallery'),
+    url(r'^categories/',views.display_images_categories,name = 'categories'),
+    url(r'^locations/',views.display_images_locations,name= 'locations'),
+    url(r'^search/',views.search_results,name = 'search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
